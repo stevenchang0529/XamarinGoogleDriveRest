9563f596ba9b3071c3776a6be59822f2bfa0eb0b
Steven <steven@MSI-Steven>
2020-04-20 17:44:37 +0800

    加入專案檔案。

---
+++ XamarinGoogleDriveRest/XamarinGoogleDriveRest.Android/ActivityCustomUrlSchemeInterceptor.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using System;
+using Xamarin.Auth;
+
+namespace XamarinGoogleDriveRest.Droid
+{
+    [Activity(Label = "ActivityCustomUrlSchemeInterceptor", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(new[] { Intent.ActionView }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable }, DataSchemes = new[] { "xamarin.test.driverest" }, DataPath = "/oauth2redirect")]
+    public class ActivityCustomUrlSchemeInterceptor : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            global::Android.Net.Uri uri_android = Intent.Data;
+            CustomTabsConfiguration.CustomTabsClosingMessage = null;
+            var uri = new Uri(Intent.Data.ToString());
+            AuthenticatorHelper.OAuth2Authenticator.OnPageLoading(uri);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+            StartActivity(intent);
+            this.Finish();
+            return;
+        }
+    }
+
+}
---
+++ XamarinGoogleDriveRest/XamarinGoogleDriveRest/ViewModels/MainViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Auth;
+using Xamarin.Auth.Presenters;
+using Xamarin.Forms;
+
+namespace XamarinGoogleDriveRest
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string scope = "https://www.googleapis.com/auth/drive.file";
+        private string clientId = "put your client Id from goole api console";
+        private string redirectUrl = "xamarin.test.driverest:/oauth2redirect";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand OnGoogleDrive { get;  set; }
+
+        public MainViewModel()
+        {
+            var auth = new OAuth2Authenticator(
+              this.clientId,
+              string.Empty,
+              scope,
+              new Uri("https://accounts.google.com/o/oauth2/v2/auth"),
+              new Uri(redirectUrl),
+              new Uri("https://www.googleapis.com/oauth2/v4/token"),
+              isUsingNativeUI: true);
+            AuthenticatorHelper.OAuth2Authenticator = auth;
+            auth.Completed +=async  (sender, e) =>
+            {
+                if (e.IsAuthenticated)
+                {
+
+                    var initializer = new GoogleAuthorizationCodeFlow.Initializer
+                    {
+                        ClientSecrets = new Google.Apis.Auth.OAuth2.ClientSecrets()
+                        {
+                            ClientId = clientId,
+                        }
+                    };
+                    initializer.Scopes = new[] { scope };
+                    initializer.DataStore = new FileDataStore("Google.Apis.Auth");
+                    var flow = new GoogleAuthorizationCodeFlow(initializer);
+                    var user = "DriveTest";
+                    var token = new TokenResponse()
+                    {
+                         AccessToken=e.Account.Properties["access_token"],
+                         ExpiresInSeconds=Convert.ToInt64( e.Account.Properties["expires_in"]),
+                         RefreshToken=e.Account.Properties["refresh_token"],
+                         Scope=e.Account.Properties["scope"],
+                         TokenType=e.Account.Properties["token_type"]
+                    };
+                    UserCredential userCredential = new UserCredential(flow, user, token);
+                    var driveService = new DriveService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = userCredential,
+                        ApplicationName = "XamarinDriveTest",
+                    });
+
+
+                    //test google drive
+                    DriveServiceHelper helper = new DriveServiceHelper(driveService);
+                    var id=await helper.CreateFile();
+                    await helper.SaveFile(id, "test", "test save content");
+                    var content =await helper.ReadFile(id);
+                }
+            };
+            auth.Error += (sender, e) =>
+            {
+
+            };
+
+            this.OnGoogleDrive = new Command(() =>
+              {
+                  var presenter = new OAuthLoginPresenter();
+                  presenter.Login(auth);
+              });
+        }
+
+
+    }
+
+    public static class AuthenticatorHelper
+    {
+        public static OAuth2Authenticator OAuth2Authenticator { get; set; }
+    }
+}
